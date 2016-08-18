82217777c605945addde357338d7d86f098cf25e
stanislas laurent <stanislas.lv@gmail.com>
2016-08-18 18:46:54 +0100

    model booking done

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     resources :challenges, only: [:create, :index, :show, :destroy]
   end
 
+  resources :bookings
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
