1d43aa8dccb97dab6ab926024ebb42c88454d23d
hkdeb <debbie.hugger@gmail.com>
2016-08-19 12:56:48 +0100

    category index with icons

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ActiveRecord::Base
   has_many :challenges
 
   after_create do |category|
-    category.picture = "category_#{category.name.downcase}.jpg"
+    category.picture = "category_#{category.name.downcase}.png"
     category.save
   end
 end
