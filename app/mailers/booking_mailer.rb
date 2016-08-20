0387aec70ffc5edc8b264031dd45bc27ddf62369
stanislas laurent <stanislas.lv@gmail.com>
2016-08-20 18:44:23 +0100

    email after challenge creation

---
+++ app/mailers/booking_mailer.rb
@@ -0,0 +1,18 @@
+class BookingMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.booking_mailer.challenge_validation.subject
+  #
+  def challenge_validation(booking)
+    @booking = booking
+    @greeting = "Hi"
+
+     mail(
+      to:       @booking.user.email,
+      subject:  "Booking created!"
+    )
+
+  end
+end
