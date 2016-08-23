56a1a836a16ee89298b9eeceacb4723f356db6a8
stanislas laurent <stanislas.lv@gmail.com>
2016-08-23 16:44:48 +0100

    loop on application controller to make sure we have the gender

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :redirect_to_gender
 
   def after_sign_in_path_for(user)
     user.gender.nil? ? gender_path : play_path
+  end
+
+  def redirect_to_gender
+    (redirect_to gender_path and return false) if user_signed_in? && current_user.gender.nil? && !(request.url.include?("gender")) && !(params[:user].nil?) && params[:user][:gender].nil?
   end
 
   protected
