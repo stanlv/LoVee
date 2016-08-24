2501c73a318baa5ee1bfe672f507071ed7b99da3
hkdeb <debbie.hugger@gmail.com>
2016-08-24 11:47:36 +0100

    footer items aligned and modal done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -22,7 +22,7 @@ class BookingsController < ApplicationController
       status: "created")
     if @booking.save
       BookingMailer.challenge_validation(@booking).deliver_now
-      redirect_to @booking, notice: 'One challenge has been picked up !'
+      redirect_to @booking, notice: 'One challenge has been assigned! Go to your dashboard to view it.'
     else
       render :new
     end
