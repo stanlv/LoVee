4dace6a2faa54e6146399f49a2294e7db6676eb2
stanislas laurent <stanislas.lv@gmail.com>
2016-08-24 19:13:52 +0100

    fix mail params

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -3,7 +3,7 @@ class InvitationsController < ApplicationController
   def invite
     # send mail with link to /users/signup?partner_id=45
     flash[:notice] = "We have invited your partner #{params[:email]}"
-    InvitationMailer.invitation(@email, current_user).deliver_now
+    InvitationMailer.invitation(params[:email], current_user).deliver_now
 
     redirect_to dashboard_path
   end
