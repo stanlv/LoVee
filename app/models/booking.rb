f83592defd859db761bb73412cf80657c037da43
stanislas laurent <stanislas.lv@gmail.com>
2016-08-25 10:30:01 +0100

    change spot for counter booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,7 @@ class Booking < ActiveRecord::Base
   belongs_to :challenge
   has_one :category, through: :challenge
 
+  validates :status, inclusion: { in: %w(created completed failed) } # ['created', 'completed', 'failed']
+
+
 end
